25b2c0f6b226bb84a3a8a34d8c1bff31b80a0365
quidprocrow <windmillwarrior@gmail.com>
2018-01-26 13:03:02 -0500

    Confirm that sign out works with correct token
    Confirm that sign out works with correct token and not without it.

--- scripts/auth/sign-out.sh
+++ scripts/auth/sign-out.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+TOKEN="BAhJIiVmMmQ0Nzg1YzM4YTQxNGQ0MzI2MTA3ZDMxN2ExOGE4MAY6BkVG--3bdda94247ad89568ec7e62370cf5b006328eaff"
 curl "http://localhost:4741/sign-out" \
   --include \
   --request DELETE \
