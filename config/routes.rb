0cf5c3c47b981cbd535d38ea974b5db6d86b9ca5
quidprocrow <windmillwarrior@gmail.com>
2018-01-26 14:50:46 -0500

    Generate lists via scaffold
    Generate lists via scaffold. Add list reference to items table.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :lists
   resources :items
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126165104) do
+ActiveRecord::Schema.define(version: 20180126194747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20180126165104) do
     t.string "archived"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_items_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +49,5 @@ ActiveRecord::Schema.define(version: 20180126165104) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "items", "lists"
 end
